872263acbca7d34bfcbc90734b92157d225a7d63
Miguel Reyes <miguel.reyes@enernoc.com>
2018-03-19 10:45:00 -0400

    Writing note to a JSON file and validate unique title
    $ node app.js add --title="My title 2" --body="Some body here"

--- notes.js
+++ notes.js
@@ -1,23 +1,41 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-  console.log('will add: ' + title + ": " + body);
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+    try {
+        notes = JSON.parse(fs.readFileSync('notes-data.json'));
+    } catch (e) {
+
+    }
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
 };
 
 var getAll = () => {
-  console.log('getting all notes');
+    console.log('getting all notes');
 };
 var getNote = (title) => {
-  console.log('read: ', title);
+    console.log('read: ', title);
 };
 var removeNote = (title) => {
-  console.log('remove: ', title);
+    console.log('remove: ', title);
 };
 
 
 module.exports = {
-  addNote,
-  getAll,
-  getNote,
-  removeNote
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 };
