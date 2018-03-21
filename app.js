2208e4f171a440fa94e67a014a2ca09daf4d0075
Miguel Reyes <miguel.reyes@enernoc.com>
2018-03-21 12:37:12 -0400

    Read note

--- app.js
+++ app.js
@@ -22,7 +22,12 @@ if (command === 'add') {
 } else if (command == 'list') {
     notes.getAll();
 } else if (command == 'read') {
-    notes.getNote(argv.title);
+    var note = notes.getNote(argv.title);
+    if(note) {
+        console.log('Note found: ' + JSON.stringify(note));
+    } else {
+        console.log('Note not found.')
+    }
 } else if (command == 'remove') {
     var noteRemoved = notes.removeNote(argv.title);
     var message = noteRemoved ? 'Note was removed' : 'Note not found';
--- notes.js
+++ notes.js
@@ -33,9 +33,13 @@ var addNote = (title, body) => {
 var getAll = () => {
     console.log('getting all notes');
 };
+
 var getNote = (title) => {
-    console.log('read: ', title);
+    var notes = fetchNodes();
+    var filteredNotes = notes.filter((note) => note.title === title);
+    return filteredNotes[0];
 };
+
 var removeNote = (title) => {
     var notes = fetchNodes();
     var notesAfterDelete = notes.filter((note) => note.title !== title);
